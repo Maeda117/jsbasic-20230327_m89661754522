7c08f69eb5f8560483952f466e833cf6daf1bf5a
Maeda117 <129417104+Maeda117@users.noreply.github.com>
2023-04-23 19:42:13 +0300

    Update index.js

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -11,15 +11,13 @@ function factorial(n) {
 
 let z = 1
 let v = x -1
-  // console.log('y:', y)
+
 
   for (let i = 0; i < v; i++) { // выведет 0, затем 1, затем 2
     y = y * (x - z)
-    // console.log(y)
+   
     z = z + 1
-    // console.log('z:', z)
-    // v = z + 3
-    // console.log(v)
+   
     console.log('y:', y)
   }
 
